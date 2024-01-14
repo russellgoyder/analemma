23d780db52ec385eeed06075bf39ebef55a622d1
Russell Goyder <russellgoyder@users.noreply.github.com>
2024-01-14 10:46:41 -0800

    Material theme with latex hack

---
+++ dollar_dollar.py
@@ -0,0 +1,30 @@
+r"""
+jupyter nbconvert --to Markdown sundial.ipynb produces markdown with latex output like this:
+
+$\displaystyle 
+        \begin{equation}
+            f_1 \wedge f_2 = \cos{\left (\alpha  \right )} \boldsymbol{e}_{1}\wedge \boldsymbol{e}_{2} - \sin{\left (\alpha  \right )} \boldsymbol{e}_{2}\wedge \boldsymbol{e}_{3} \nonumber
+        \end{equation}
+        $
+
+However, environments such as equation and align require math mode, which this script
+hacks into place within the ipynb file before converting to markdown.
+"""
+
+import json
+
+with open("sundial.ipynb", mode= "r", encoding= "utf-8") as f:
+    doc = json.loads(f.read())
+
+for cell in doc["cells"]:
+    if cell["cell_type"] == "code":
+        for output in cell["outputs"]:
+            if "data" in output.keys():
+                if "text/latex" in output["data"]:
+                    latex_lines = output["data"]["text/latex"]
+                    if latex_lines[0].startswith("$") and latex_lines[-1].endswith("$"):
+                        latex_lines[0] = "$" + latex_lines[0]
+                        latex_lines[-1] = latex_lines[-1] + "$"
+
+with open("sundial_dollar_dollar.ipynb", mode= "w") as f:
+    f.write(json.dumps(doc))
