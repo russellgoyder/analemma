b201a98e3f6d630b0b16488fdd0e8b4689ef96ea
Russell Goyder <russellgoyder@users.noreply.github.com>
2024-09-21 07:45:56 -0700

    Better name for doc build script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,5 +2,5 @@
 
 git checkout docs/sundial.ipynb
 rm -rf docs/*_files
-rm -f docs/sundial.md docs/equation_of_time.md docs/sundial_plots.md
+rm -f docs/sundial.md docs/equation_of_time.md docs/sundial_plots.md docs/orbit_analysis.md
 find . -name .DS_Store -delete
