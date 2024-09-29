c0171514f84abebfc71ad4451361887717064896
Russell Goyder <russellgoyder@users.noreply.github.com>
2024-09-28 17:02:35 -0700

    Added safety check in doc build script

--- scripts/build_doc.sh
+++ scripts/build_doc.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+n=$(git status docs/nb --porcelain=1 | wc -l)
+if [ $n -ne 0 ]
+then 
+    echo Error: Cannot build doc with uncommitted change to notebooks 1>&2
+    exit 1
+fi
+
 for x in $(ls docs/nb/*.ipynb); do python scripts/dollar_dollar.py $x $x; done
+
 find docs/nb -type 'f' -name '*.ipynb' -not -name "*-checkpoint.ipynb" | xargs jupyter nbconvert --to Markdown
 mkdocs serve
