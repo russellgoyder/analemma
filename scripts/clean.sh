51d5e0d4d10dcd842ea9e8e555ea2ffad5f235cb
Russell Goyder <russellgoyder@users.noreply.github.com>
2024-10-27 19:37:10 -0700

    Quieter

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,4 +4,4 @@ git checkout docs/nb/*.ipynb # undo scripts/dollar_dollar.py
 rm -rf docs/nb/*_files
 rm -f docs/nb/*.md
 find . -name .DS_Store -delete
-rm *.bsp docs/nb/*.bsp # ephemeris files from skyfield
+rm -f *.bsp docs/nb/*.bsp # ephemeris files from skyfield
